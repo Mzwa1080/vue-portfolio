78864428bcf9ddf80a80ff0d1ace4f3c1476b0a4
Mzwa1080 <mshawnlouw@gmail.com>
2024-01-09 16:32:51 +0200

    added animation on home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,18 @@
   <div >
 
     <div class="row vh-100 my-5">
-      <div class="col">
+      <div class="col animate__animated animate__fadeInLeft">
         <img class="img-fluid" src="https://lh3.google.com/u/0/d/17WtuDAJiN9upGTHqdDj5xclcPWV0gCOE=w672-h504-p-k-nu-iv1" alt="">
       </div>
 
-      <div class="col">
+      <div class="col animate__animated animate__fadeInRight">
         
         <h2 class="mt-5 pt-5 me-5">Hi, Welcome</h2> <br>
       
         <h2 class="center m">To</h2>
       
       
-      <h2 class="mt-5"> Mzwamadoda Louw's <br>
+      <h2 class="mt-5 "> Mzwamadoda Louw's <br>
          Portfolio</h2>
 
          <!-- {{ $store.state.about }} -->
