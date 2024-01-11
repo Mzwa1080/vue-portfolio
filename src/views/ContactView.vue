d2e32bc4d05127d6c403b69f990263316e2d7398
Mzwa1080 <mshawnlouw@gmail.com>
2024-01-11 08:34:42 +0200

    fied the contact page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -2,20 +2,20 @@
   <div>
     <h1 class="mt-3">Contact Me</h1>
 
-    <div class="row mb-5 row-box ">
+    <div class="row mb-5  row-box ">
 
-        <div class="col-6 grid gap-3">
+        <div class="col-6 my-4 grid gap-3">
 
             <form class=" rounded " action="https://formspree.io/f/xzbnzbzd" method="post">
     
                 <div class=" col mb-3">
                   <label for="exampleFormControlInput1" class="form-label">Full Name</label>
-                  <input type="text" name="" class="form-control" id="exampleFormControlInput1" placeholder="John Doe"        />
+                  <input type="text" name="fullName" class="form-control" id="exampleFormControlInput1" placeholder="John Doe"        />
                 
               </div>
               <div class=" col mb-3">
                   <label for="exampleFormControlInput2" class="form-label">Email</label>
-                  <input type="email"  name="" class="form-control" id="exampleFormControlInput2" placeholder="mshawnlouw@gmail.com"        />
+                  <input type="email"  name="email" class="form-control" id="exampleFormControlInput2" placeholder="mshawnlouw@gmail.com"        />
                 
               </div>
     
@@ -32,6 +32,7 @@
                       class="form-control"
                       id="exampleFormControlTextarea1"
                       rows="3"
+                      name="message"
                     ></textarea>
                   </div>
               </div>
@@ -43,8 +44,8 @@
             </form>
         </div>
 
-        <div class="col-6">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3306.0176997194853!2d18.65344317571607!3d-34.0434170731603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc48da4b8803b9%3A0x93f212ab5f4aa1b8!2sNqawe%20St%2C%20Village%201%20North%2C%20Cape%20Town%2C%207784!5e0!3m2!1sen!2sza!4v1704896316386!5m2!1sen!2sza" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div class="col-6 my-4">
+            <iframe :width="450" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3306.0176997194853!2d18.65344317571607!3d-34.0434170731603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc48da4b8803b9%3A0x93f212ab5f4aa1b8!2sNqawe%20St%2C%20Village%201%20North%2C%20Cape%20Town%2C%207784!5e0!3m2!1sen!2sza!4v1704896316386!5m2!1sen!2sza"  :height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
     </div>
